91feb098a367e6c428196aa6aa99d40c39e47354
shaaibu7 <shaaibusuleiman9@gmail.com>
2026-02-15 08:53:42 +0100

    feat: expose functions to retrieve XMSS public key and secret key

--- rust/leansig/src/lib.rs
+++ rust/leansig/src/lib.rs
@@ -1,17 +1,17 @@
 use leansig::signature::SignatureScheme;
 use leansig::signature::generalized_xmss::instantiations_poseidon_top_level::lifetime_2_to_the_32::hashing_optimized::SIGTopLevelTargetSumLifetime32Dim64Base8;
 use rand::{SeedableRng,rngs::StdRng};
+use std::ptr;
 
 pub type LeanSignatureScheme = SIGTopLevelTargetSumLifetime32Dim64Base8;
 pub type LeanPublicKey = <LeanSignatureScheme as SignatureScheme>::PublicKey;
 pub type LeanSecretKey = <LeanSignatureScheme as SignatureScheme>::SecretKey;
 
-#[repr(C)]
+
 pub struct SecretKey {
     pub inner: LeanSecretKey,
 }
 
-#[repr(C)]
 pub struct PublicKey {
     pub inner: LeanPublicKey,
 }
@@ -58,4 +58,50 @@ pub unsafe extern "C" fn leansig_keypair_generate(
     });
     
     Box::into_raw(keypair)
+}
+
+// Get a pointer to the public key from a keypair
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn leansig_keypair_get_public_key(keypair: *const Keypair) -> *const PublicKey {
+    if keypair.is_null() {
+           return ptr::null();
+    }
+    
+    unsafe {
+         &(*keypair).public_key
+    }
+}
+
+// Get a pointer to the secret key from a keypair
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn leansig_keypair_get_private_key(keypair: *const Keypair) -> *const SecretKey {
+    if keypair.is_null() {
+           return ptr::null();
+    }
+    
+    unsafe {
+         &(*keypair).secret_key
+    }
+}
+
+
+/// FFI: Frees a heap-allocated XMSS `Keypair`.
+///
+/// # Safety
+/// - `key_pair` must be a pointer previously returned by
+///   `leansig_generate_keypair` (or any function that allocates a `Keypair` on the heap).
+/// - Passing a null pointer is safe (function does nothing).
+/// - After calling this function, the pointer must not be used again.
+/// - Must only be called once per allocated `Keypair` to avoid double-free.
+///
+/// # Notes
+/// - This function is intended for use from Go or other languages via FFI.
+/// - It converts the raw pointer back into a `Box` and drops it, freeing the memory.
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn leansig_keypair_free(key_pair: *mut Keypair) {
+    if !key_pair.is_null() {
+        unsafe {
+            let _ = Box::from_raw(key_pair);
+        }
+    }
 }
