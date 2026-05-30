cbda756ab15535fbe697a11f79fce077b60d65a3
Longs Pemun Gotar <developerlongs@gmail.com>
2026-05-30 10:09:01 +0100

    node: gofmt aggregation_pool.go and metrics.go
    Pre-existing gofmt violations inherited from devnet-4 (declaration
    alignment and a misindented metrics var) that fail the repo-wide
    gofmt -l CI check. Formatting only; no behavior change.

--- node/aggregation_pool.go
+++ node/aggregation_pool.go
@@ -17,14 +17,14 @@ var (
 	rawIDsPool      = sync.Pool{New: func() any { s := make([]uint64, 0, 32); return &s }}
 )
 
-func getChildProofsBuf() *[]xmss.ChildProof { return childProofsPool.Get().(*[]xmss.ChildProof) }
+func getChildProofsBuf() *[]xmss.ChildProof  { return childProofsPool.Get().(*[]xmss.ChildProof) }
 func putChildProofsBuf(b *[]xmss.ChildProof) { *b = (*b)[:0]; childProofsPool.Put(b) }
 
-func getRawPubkeysBuf() *[]xmss.CPubKey { return rawPubkeysPool.Get().(*[]xmss.CPubKey) }
+func getRawPubkeysBuf() *[]xmss.CPubKey  { return rawPubkeysPool.Get().(*[]xmss.CPubKey) }
 func putRawPubkeysBuf(b *[]xmss.CPubKey) { *b = (*b)[:0]; rawPubkeysPool.Put(b) }
 
-func getRawSigsBuf() *[]xmss.CSig { return rawSigsPool.Get().(*[]xmss.CSig) }
+func getRawSigsBuf() *[]xmss.CSig  { return rawSigsPool.Get().(*[]xmss.CSig) }
 func putRawSigsBuf(b *[]xmss.CSig) { *b = (*b)[:0]; rawSigsPool.Put(b) }
 
-func getRawIDsBuf() *[]uint64 { return rawIDsPool.Get().(*[]uint64) }
+func getRawIDsBuf() *[]uint64  { return rawIDsPool.Get().(*[]uint64) }
 func putRawIDsBuf(b *[]uint64) { *b = (*b)[:0]; rawIDsPool.Put(b) }
